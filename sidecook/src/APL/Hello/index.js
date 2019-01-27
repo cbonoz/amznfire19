6af2d08ffd3ea0e60a5f7b3616101cab19f46f46
JBarna <joel.tbarna@gmail.com>
2019-01-27 15:05:58 -0500

    Push up welcome

--- sidecook/src/APL/Hello/index.js
+++ sidecook/src/APL/Hello/index.js
@@ -1,5 +1,14 @@
 module.exports = (payload) => {
   const {appName, appTip} = payload
+
+  const data = {
+    backgroundSmall: "https://d2o906d8ln7ui1.cloudfront.net/images/LT1_Background.png",
+    backgroundLarge: "https://d2o906d8ln7ui1.cloudfront.net/images/LT1_Background.png",
+    "title": "Welcome to SideCook!",
+    "logoUrl": "https://raw.githubusercontent.com/cbonoz/amznfire19/master/img/side_cook_white.png"
+  }
+
+
   return {
     "type": "APL",
     "version": "1.0",
@@ -15,9 +24,26 @@ module.exports = (payload) => {
       ],
       "items": [
         {
-          "type": "Text",
-          "text": `Welcome to ${appName} ${appTip}`
-        },
+          "type": "Container",
+          "width": "100vw",
+          "height": "100vh",
+          "direction": "column",
+          "items": [
+            {
+                "type": "Image",
+                "source": `${data.backgroundLarge}`,
+                "scale": "best-fill",
+                "width": "100vw",
+                "height": "100vh",
+                "position": "absolute"
+            },
+            {
+                "type": "AlexaHeader",
+                "headerTitle": `${data.title}`,
+                "headerAttributionImage": `${data.logoUrl}`
+            }
+          ]
+        }
       ]
     }
   }
