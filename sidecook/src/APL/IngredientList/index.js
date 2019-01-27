02fd53a3fd60c94cf633b3002a76f01ca6f9ad57
JBarna <joel.tbarna@gmail.com>
2019-01-27 17:05:07 -0500

    Added ingredient list baby!

--- sidecook/src/APL/IngredientList/index.js
+++ sidecook/src/APL/IngredientList/index.js
@@ -1,3 +1,5 @@
+const helper = require('../helpers')
+
 module.exports = payload => {
   const { name, ingredients, category, region, thumb } = payload;
 
@@ -7,131 +9,7 @@ module.exports = payload => {
     token: 'recipe-ingredients-doc',
     version: '1.0',
     document: {
-      type: "APL",
-      version: "1.0",
-      theme: "dark",
-      import: [
-        {
-          name: "alexa-layouts",
-          version: "1.0.0"
-        }
-      ],
-      resources: [
-        {
-          description: "Stock color for the light theme",
-          colors: {
-            colorTextPrimary: "#151920"
-          }
-        },
-        {
-          description: "Stock color for the dark theme",
-          when: "${viewport.theme == 'dark'}",
-          colors: {
-            colorTextPrimary: "#f0f1ef"
-          }
-        },
-        {
-          description: "Standard font sizes",
-          dimensions: {
-            textSizeBody: 48,
-            textSizePrimary: 27,
-            textSizeSecondary: 23,
-            textSizeDetails: 20,
-            textSizeSecondaryHint: 25
-          }
-        },
-        {
-          description: "Common spacing values",
-          dimensions: {
-            spacingThin: 6,
-            spacingSmall: 12,
-            spacingMedium: 24,
-            spacingLarge: 48,
-            spacingExtraLarge: 72
-          }
-        },
-        {
-          description: "Common margins and padding",
-          dimensions: {
-            marginTop: 40,
-            marginLeft: 60,
-            marginRight: 60,
-            marginBottom: 40
-          }
-        }
-      ],
-      styles: {
-        textStyleBase: {
-          description: "Base font description; set color and core font family",
-          values: [
-            {
-              color: "@colorTextPrimary",
-              fontFamily: "Amazon Ember"
-            }
-          ]
-        },
-        textStyleBase0: {
-          description: "Thin version of basic font",
-          extend: "textStyleBase",
-          values: {
-            fontWeight: "100"
-          }
-        },
-        textStyleBase1: {
-          description: "Light version of basic font",
-          extend: "textStyleBase",
-          values: {
-            fontWeight: "300"
-          }
-        },
-        textStyleBase2: {
-          description: "Regular version of basic font",
-          extend: "textStyleBase",
-          values: {
-            fontWeight: "500"
-          }
-        },
-        mixinBody: {
-          values: {
-            fontSize: "@textSizeBody"
-          }
-        },
-        mixinPrimary: {
-          values: {
-            fontSize: "@textSizePrimary"
-          }
-        },
-        mixinDetails: {
-          values: {
-            fontSize: "@textSizeDetails"
-          }
-        },
-        mixinSecondary: {
-          values: {
-            fontSize: "@textSizeSecondary"
-          }
-        },
-        textStylePrimary: {
-          extend: ["textStyleBase1", "mixinPrimary"]
-        },
-        textStyleSecondary: {
-          extend: ["textStyleBase0", "mixinSecondary"]
-        },
-        textStyleBody: {
-          extend: ["textStyleBase1", "mixinBody"]
-        },
-        textStyleSecondaryHint: {
-          values: {
-            fontFamily: "Bookerly",
-            fontStyle: "italic",
-            fontSize: "@textSizeSecondaryHint",
-            color: "@colorTextPrimary"
-          }
-        },
-        textStyleDetails: {
-          extend: ["textStyleBase2", "mixinDetails"]
-        }
-      },
+      ...helper.base(),
       layouts: {
         ListTemplate1: {
           parameters: [
@@ -148,19 +26,7 @@ module.exports = payload => {
               height: "100vh",
               direction: "column",
               items: [
-                {
-                  type: "Image",
-                  source: "${backgroundImage}",
-                  scale: "best-fill",
-                  width: "100vw",
-                  height: "100vh",
-                  position: "absolute"
-                },
-                {
-                  type: "AlexaHeader",
-                  headerTitle: "${title}",
-                  headerAttributionImage: "${logo}"
-                },
+                ...helper.header('Ingredients for ' + name),
                 {
                   type: "Sequence",
                   grow: 1,
@@ -168,15 +34,11 @@ module.exports = payload => {
                   scrollDirection: "vertical",
                   paddingLeft: "@marginLeft",
                   paddingRight: "@marginRight",
-                  data: "${listData}",
+                  data: ingredients,
                   numbered: true,
                   items: [
                     {
-                      type: "VerticalListItem",
-                      image: "${data.image.sources[0].url}",
-                      primaryText: "${data.textContent.primaryText.text}",
-                      secondaryText: "${data.textContent.secondaryText.text}",
-                      tertiaryText: "${data.textContent.tertiaryText.text}"
+                      type: "VerticalListItem"
                     }
                   ]
                 }
@@ -185,7 +47,6 @@ module.exports = payload => {
           ]
         },
         VerticalListItem: {
-          parameters: ["primaryText", "secondaryText", "tertiaryText", "image"],
           item: [
             {
               when: "${viewport.shape == 'round'}",
@@ -260,19 +121,8 @@ module.exports = payload => {
                 {
                   type: "Text",
                   text: "${ordinal}",
-                  paddingBottom: "20dp",
                   color: "white",
                   spacing: "5dp"
-                },
-                {
-                  type: "Image",
-                  when: "${image}",
-                  source: "${image}",
-                  height: 100,
-                  width: 100,
-                  align: "top",
-                  spacing: 50,
-                  scale: "best-fit"
                 },
                 {
                   type: "Container",
@@ -281,17 +131,8 @@ module.exports = payload => {
                   items: [
                     {
                       type: "Text",
-                      text: "${primaryText}",
+                      text: "${data.name}",
                       style: "textStyleBody",
-                      fontWeight: "300",
-                      grow: 1,
-                      shrink: 1,
-                      maxLines: 1
-                    },
-                    {
-                      type: "Text",
-                      text: "${secondaryText}",
-                      style: "textStyleDetails",
                       fontWeight: "300",
                       grow: 1,
                       shrink: 1,
@@ -301,7 +142,7 @@ module.exports = payload => {
                 },
                 {
                   type: "Text",
-                  text: "${tertiaryText}",
+                  text: "${data.measure}",
                   style: "textStyleBody",
                   fontWeight: "300",
                   grow: 1,
@@ -318,45 +159,12 @@ module.exports = payload => {
         parameters: ["payload"],
         item: [
           {
-            type: "ListTemplate1",
-            backgroundImage:
-              "${payload.listTemplate1Metadata.backgroundImage.sources[0].url}",
-            title: "${payload.listTemplate1Metadata.title}",
-            logo: "${payload.listTemplate1Metadata.logoUrl}",
-            listData: "${payload.listTemplate1ListData.listPage.listItems}"
+            type: "ListTemplate1"
           }
         ]
       }
     },
     dataSources: {
-      listTemplate1Metadata: {
-        type: "object",
-        objectId: "lt1Metadata",
-        backgroundImage: {
-          contentDescription: null,
-          smallSourceUrl: null,
-          largeSourceUrl: null,
-          sources: [
-            {
-              url:
-                "https://d2o906d8ln7ui1.cloudfront.net/images/LT1_Background.png",
-              size: "small",
-              widthPixels: 0,
-              heightPixels: 0
-            },
-            {
-              url:
-                "https://d2o906d8ln7ui1.cloudfront.net/images/LT1_Background.png",
-              size: "large",
-              widthPixels: 0,
-              heightPixels: 0
-            }
-          ]
-        },
-        title: "Calories in 1 Serving of Cheese",
-        logoUrl:
-          "https://d2o906d8ln7ui1.cloudfront.net/images/cheeseskillicon.png"
-      },
       listTemplate1ListData: {
         type: "list",
         listId: "lt1Sample",
